8a85b1c0b07e551269965414c76ef1cfed1668e1
Arianna Perez <akdev.aring@gmail.com>
2025-06-18 23:27:22 +0800

    Create Navbar component

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default async function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      {children}
+    </div>
+  )
+}
---
+++ src/app/(consumer)/layout.tsx
@@ -0,0 +1,48 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs"
+import Link from "next/link"
+import { Suspense } from "react"
+import { UserButton } from "@clerk/nextjs"
+import { Button } from "@/components/ui/button"
+import { SignInButton } from "@clerk/nextjs"
+
+export default function ConsumerLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+   <div>
+    <Navbar />
+    {children}
+    </div>
+  )
+}
+
+function Navbar() {
+  return (
+    <header className="flex h-12 shadow bg-background z-10">
+  <nav className="flex gap-4 container">
+    <Link className="mr-auto text-lg hover:underline px-2 flex items-center" href="/">Web Dev Simplified</Link>
+    <Suspense fallback={<div>Loading...</div>}>
+
+    <SignedIn>
+      <Link className="hover:bg-accent/10 flex items-center px-2" href="/">Admin</Link>
+      <Link className="hover:bg-accent/10 flex items-center px-2" href="/courses">My Courses</Link>
+      <Link className="hover:bg-accent/10 flex items-center px-2" href="/purchases">Purchase History</Link>
+      <div className="size-8 self-center">
+        <UserButton  
+        appearance={
+        {  elements: {
+            userButtonAvatarBox: {width: "100%", height: "100%"}
+          },
+        }} />
+      </div>
+    </SignedIn>
+    </Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignedOut>
+        <Button className="self-center" asChild>
+          <SignInButton>Sign In</SignInButton>
+        </Button>
+      </SignedOut>
+    </Suspense>
+  </nav>
+    </header>
+  )
+}
