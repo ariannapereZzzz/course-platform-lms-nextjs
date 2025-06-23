08f06c36db17879eb6989e5f045ad70bde342780
Arianna Perez <akdev.aring@gmail.com>
2025-06-24 04:01:41 +0800

    [Admin] Create admin Course Table Page

--- src/app/admin/courses/page.tsx
+++ src/app/admin/courses/page.tsx
@@ -1,8 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { PageHeader } from "@/components/PageHeader";
 import Link from "next/link";
+import { CourseTable } from "@/features/courses/components/CourseTable";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { getCourseGlobalTag } from "@/features/courses/db/cache/courses";
+import { db } from "@/drizzle/db";
+import {
+  CourseSectionTable,
+  CourseTable as DbCourseTable,
+  LessonTable,
+  UserCourseAccessTable,
+} from "@/drizzle/schema";
+import { asc, countDistinct, eq } from "drizzle-orm";
+import { getUserCourseAccessGlobalTag } from "@/features/courses/db/cache/userCourseAccess";
+import { getCourseSectionGlobalTag } from "@/features/courseSections/db/cache";
+import { getLessonGlobalTag } from "@/features/lessons/db/cache/lessons";
+import { Suspense } from "react";
 
-export default function CoursesPage() {
+export default async function CoursesPage() {
+  const courses = await getCourses();
+
   return (
     <div className="container my-6">
       <PageHeader title="Courses">
@@ -10,7 +27,41 @@ export default function CoursesPage() {
           <Link href="/admin/courses/new">New Course</Link>
         </Button>
       </PageHeader>
-      <div>Courses Table</div>
+
+      <Suspense fallback={<div>Loading courses...</div>}>
+        <CourseTable courses={courses} />
+      </Suspense>
     </div>
   );
+}
+
+async function getCourses() {
+  "use cache";
+  cacheTag(
+    getCourseGlobalTag(),
+    getUserCourseAccessGlobalTag(),
+    getCourseSectionGlobalTag(),
+    getLessonGlobalTag()
+  );
+
+  return db
+    .select({
+      id: DbCourseTable.id,
+      name: DbCourseTable.name,
+      sectionsCount: countDistinct(CourseSectionTable),
+      lessonsCount: countDistinct(LessonTable),
+      studentsCount: countDistinct(UserCourseAccessTable),
+    })
+    .from(DbCourseTable)
+    .leftJoin(
+      CourseSectionTable,
+      eq(CourseSectionTable.courseId, DbCourseTable.id)
+    )
+    .leftJoin(LessonTable, eq(LessonTable.sectionId, CourseSectionTable.id))
+    .leftJoin(
+      UserCourseAccessTable,
+      eq(UserCourseAccessTable.courseId, DbCourseTable.id)
+    )
+    .orderBy(asc(DbCourseTable.name))
+    .groupBy(DbCourseTable.id);
 }
--- src/features/courses/actions/courses.ts
+++ src/features/courses/actions/courses.ts
@@ -4,8 +4,8 @@ import z from "zod";
 import { courseSchema } from "../schemas/courses";
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/services/clerk";
-import { canCreateCourses } from "../permissions/courses";
-import { insertCourse } from "../db/courses";
+import { canCreateCourses, canDeleteCourses } from "../permissions/courses";
+import { insertCourse, deleteCourse as deleteCourseDB } from "../db/courses";
 
 export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
   const { success, data } = courseSchema.safeParse(unsafeData);
@@ -17,4 +17,12 @@ export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
   const course = await insertCourse(data);
 
   redirect(`/admin/courses/${course.id}/edit`);
+}
+export async function deleteCourse(id: string) {
+  if (!canDeleteCourses(await getCurrentUser())) {
+    return { error: true, message: "Error deleting your course" };
+  }
+
+  await deleteCourseDB(id);
+  return { error: false, message: "Successfully deleted your course" };
 }
